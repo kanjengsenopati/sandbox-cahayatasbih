c80307c0c36664c71f873eb9eb557249b782afc9
Dian Rusdianto <dianrusdianto06@gmail.com>
2024-05-12 22:53:16 +0700

    fix: fix status on callback

--- app/Http/Controllers/Api/V1/TransactionController.php
+++ app/Http/Controllers/Api/V1/TransactionController.php
@@ -162,7 +162,7 @@ class TransactionController extends Controller
                 } elseif ($transaction->type == Transaction::TYPE_PPDB) {
                     foreach ($transaction->transactionDetails as $detail) {
                         $detail->ppdbRegistration->update([
-                            'status' => PpdbRegistration::STATUS_PAID
+                            'payment_status' => PpdbRegistration::STATUS_PAID
                         ]);
                     }
                 } else {
