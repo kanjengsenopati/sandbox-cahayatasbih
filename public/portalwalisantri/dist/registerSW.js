48c267464eb76e5b19b703b27b0e93cd238ef890
Siswanto <riansetiyawanskom@gmail.com>
2026-05-12 19:34:11 +0700

    build: include production assets in public directory

---
+++ public/portalwalisantri/dist/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/portalwalisantri/dist/client/sw.js', { scope: '/portalwalisantri/dist/client/' })})}
--- public/portalwalisantri/dist/server/index.js
+++
@@ -1,5 +0,0 @@
-import { w } from "./assets/worker-entry-zXm__Ov7.js";
-import "node:events";
-export {
-  w as default
-};
