f9b7c8c964d0c396030370e6ba3d55e8055f2ae5
Siswanto <riansetiyawanskom@gmail.com>
2026-09-01 19:27:55 +0700

    fix: Suppress PHP 8.4 deprecation warnings in audit standalone script

--- public/audit_vps.php
+++ public/audit_vps.php
@@ -5,6 +5,10 @@
  * FILTER: Hanya untuk Tahun Ajaran 2026/2027 (Semua UPT)
  */
 
+// --- SUPPRESS DEPRECATION WARNINGS FOR VPS ---
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
+ini_set('display_errors', '0');
+
 require __DIR__.'/../vendor/autoload.php';
 $app = require_once __DIR__.'/../bootstrap/app.php';
 $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
